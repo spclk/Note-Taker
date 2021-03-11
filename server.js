ce7d02349bfedc857044153eb4798750fa6d66bc
Konstantin <konstantin1020@gmail.com>
2021-03-11 15:38:44 -0800

    final commit

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.use(express.static('public'));
 // Empty array for storing all notes
 const allNotes = [];
 
-// Setting up IDs
-let newID = uuidv4();
-
 // GET Routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
@@ -31,10 +28,24 @@ app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     const dB = fs.readFileSync(path.join(__dirname, './db/db.json'),'utf8');
     const parsedDB = JSON.parse(dB);
+    let newID = uuidv4();
+    newNote.id = newID;
     parsedDB.push(newNote);
     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parsedDB));
     return res.json(parsedDB);
   });
+
+// DELETE route to delete specified notes
+app.delete('/api/notes/:id', (req, res) => {
+ 
+  const deleteDB = fs.readFileSync(path.join(__dirname, './db/db.json'),'utf8');
+  const parsedDeleteDB = JSON.parse(deleteDB);
+  const newArray = parsedDeleteDB.filter((note)=> {
+    return req.params.id !== note.id;
+  })
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(newArray));
+  return res.json(newArray);
+});
 
 // Server listener
 app.listen(PORT, () => {
